f1d3d7941d9f90ea587dc5b3cbe184b358e5168b
root <rohits@heaptrace.com>
2019-04-08 15:43:25 +0530

    update test case

--- app/app_test.go
+++ app/app_test.go
@@ -3,16 +3,17 @@ package app
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/gorilla/mux"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+
+	"github.com/gorilla/mux"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-//Set
+//Test Set method with valid data
 var _ = Describe("Set Memcached", func() {
 	os.Setenv("MEMCACHED_HOST", "192.168.0.61")
 	os.Setenv("MEMCACHED_PORT", "11211")
@@ -37,14 +38,39 @@ var _ = Describe("Set Memcached", func() {
 	})
 })
 
-//Get
+//Test Set method with Invalid data
+var _ = Describe("Set Memcached", func() {
+	os.Setenv("MEMCACHED_HOST", "192.168.0.61")
+	os.Setenv("MEMCACHED_PORT", "11211")
+
+	setMemcached := []byte(`{Key: "Hello", Value: "World"}`)
+	requestBody := new(bytes.Buffer)
+	json.NewEncoder(requestBody).Encode(setMemcached)
+	req, err := http.NewRequest("POST", "/set", requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(SetMemcached)
+	handler.ServeHTTP(recorder, req)
+
+	Describe("Set", func() {
+		Context("Set Memcached", func() {
+			It("Should result http.StatusBadRequest", func() {
+				Expect(http.StatusBadRequest).To(Equal(recorder.Code))
+			})
+		})
+	})
+})
+
+//Test Get method with valid data
 var _ = Describe("Get Memcached", func() {
 	os.Setenv("MEMCACHED_HOST", "192.168.0.61")
 	os.Setenv("MEMCACHED_PORT", "11211")
 
-	setMemcached := ArgumentData{Key: "Hello"}
+	getMemcached := ArgumentData{Key: "Hello"}
 	requestBody := new(bytes.Buffer)
-	json.NewEncoder(requestBody).Encode(setMemcached)
+	json.NewEncoder(requestBody).Encode(getMemcached)
 	req, err := http.NewRequest("POST", "/get/key", requestBody)
 	if err != nil {
 		log.Fatal(err)
@@ -55,13 +81,43 @@ var _ = Describe("Get Memcached", func() {
 
 	req = mux.SetURLVars(req, vars)
 	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(SetMemcached)
+	handler := http.HandlerFunc(GetMemcached)
 	handler.ServeHTTP(recorder, req)
 
 	Describe("Set", func() {
 		Context("Set Memcached", func() {
 			It("Should result http.StatusOK", func() {
 				Expect(http.StatusOK).To(Equal(recorder.Code))
+			})
+		})
+	})
+})
+
+//Test Get method with Invalid data
+var _ = Describe("Get Memcached", func() {
+	os.Setenv("MEMCACHED_HOST", "192.168.0.61")
+	os.Setenv("MEMCACHED_PORT", "11211")
+
+	getMemcached := ArgumentData{Key: "Hello"}
+	requestBody := new(bytes.Buffer)
+	json.NewEncoder(requestBody).Encode(getMemcached)
+	req, err := http.NewRequest("POST", "/get/key", requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	vars := map[string]string{
+		"key": "true",
+	}
+
+	req = mux.SetURLVars(req, vars)
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetMemcached)
+	handler.ServeHTTP(recorder, req)
+
+	Describe("Set", func() {
+		Context("Set Memcached", func() {
+			It("Should result http.StatusBadRequest", func() {
+				Expect(http.StatusBadRequest).To(Equal(recorder.Code))
 			})
 		})
 	})
